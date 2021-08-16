35e56228e2dcad119cf30fa0114adb261960e8a1
Nikita <45002477+NikitaChirukhin@users.noreply.github.com>
2021-08-16 22:45:59 +0300

    Update Contents.swift

--- Lesson4/lesson4.playground/Contents.swift
+++ Lesson4/lesson4.playground/Contents.swift
@@ -2,67 +2,43 @@ protocol MultiSetProtocol {
     associatedtype T
     mutating func add(element: T)
     mutating func remove(element: T)
-    func count(of element: T) -> Int
+    func count(element: T) -> Int
     func totalCount() -> Int
 }
 
 struct MultiSet<T: Hashable>: MultiSetProtocol {
     
-    private var set = Set<[T: Int]>()
+    private var dict = [T: Int]()
     
     mutating func add(element: T) {
-        if contains(element: element) {
-            set.forEach { dictionary in
-                if let count = dictionary[element] {
-                    set.remove(dictionary)
-                    set.insert([element: count + 1])
-                }
-            }
+        if contains(key: element) {
+            dict[element]! += 1
         } else {
-            set.insert([element: 1])
+            dict[element] = 1
         }
     }
     
     mutating func remove(element: T) {
-        if contains(element: element) {
-            set.forEach { dictionary in
-                if let count = dictionary[element] {
-                    set.remove(dictionary)
-                    if count != 1 {
-                        set.insert([element: count - 1])
-                    }
-                }
+        if contains(key: element) {
+            if dict[element] == 1 {
+                dict[element] = nil
+                return
             }
+            dict[element]! -= 1
         } else {
             print("No such element")
         }
     }
     
-    func count(of element: T) -> Int {
-        var count = 0
-        set.forEach { dictionary in
-            if let num = dictionary[element] {
-                count = num
-            }
-        }
-        return count
+    func count(element: T) -> Int {
+        return dict[element] ?? 0
     }
     
     func totalCount() -> Int {
-        var count = 0
-        set.forEach { dictionary in
-            let num = dictionary.values.reduce(0, +)
-            count += num
-        }
-        return count
+        return Array(dict.values).reduce(0, +)
     }
     
-    private func contains(element: T) -> Bool {
-        set.contains(where: { dictionary in
-            if dictionary[element] != nil {
-                return true
-            }
-            return false
-        })
+    private func contains(key: T) -> Bool {
+        return dict[key] != nil
     }
 }
